cc4d69c42e2fb3cee1066eb839e0baa9c3dbf0c3
catalin c <deploy@travis-ci.org>
2020-05-17 19:51:01 +0000

    Deploy catc/should-i-watch-this to github.com/catc/should-i-watch-this.git:gh-pages

---
+++ precache-manifest.ad22905988887139b6eb2f427f92404e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f601b06240cb82992d1f877f867a4288",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dece386e29e1d595a52c",
+    "url": "./static/css/main.2f892ec5.chunk.css"
+  },
+  {
+    "revision": "37b3dd75182e6529b2db",
+    "url": "./static/js/2.48155927.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.48155927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dece386e29e1d595a52c",
+    "url": "./static/js/main.45308bff.chunk.js"
+  },
+  {
+    "revision": "4aeb7c09e1116964243f",
+    "url": "./static/js/runtime-main.75cf8cfb.js"
+  },
+  {
+    "revision": "1365e1e57abf64a94123d29d6efda133",
+    "url": "./static/media/cycle.1365e1e5.svg"
+  },
+  {
+    "revision": "f9737d3ed2e6a8b07c15ff443e9d173d",
+    "url": "./static/media/magnifying-glass.f9737d3e.svg"
+  }
+]);
